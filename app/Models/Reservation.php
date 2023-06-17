9de991838b7b22b71d845d218800702e63363bcf
Sebastian Nieroda <snieroda86@gmail.com>
2023-06-17 18:33:22 +0200

    Dodanie modelu Payments

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Reservation extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'reservation_date',
+        'reservation_time',
+        'status' ,
+        'payment_status'
+    ];
 }
